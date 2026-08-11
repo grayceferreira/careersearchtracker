8f0174d6d180bf394115fef9754423f8be5e5149
Grayce Ferreira <grayce.ferreira@gmail.com>
2026-08-11 09:48:42 -0700

    Update server.ts - Applications

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from "express";
+import { applications } from "./data/applicationRepository";
 
 const app = express();
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Career Tracker API");
-});
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Running on port 3000");
-});
-``
+app.post(
+  "/applications",
+  (req: Request, res: Response) => {
+    applications.push(req.body);
+
+    res.status(201).json(req.body);
+  }
+);
+
+app.listen(3000);
